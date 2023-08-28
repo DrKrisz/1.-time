806be59771885ab631717bbee7a7397654508169
Krisztian Bernath <70200514+DrKrisz@users.noreply.github.com>
2023-08-28 13:02:06 +0200

    js

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
 //.then(data => {
 //const publicIP = data.origin;
 //console.log('Public IP:', publicIP);
-
 //alert("Your ip is " + publicIP);
 //})
 //.catch(error => {
 //console.error('Error:', error);
 //});
 
-var ipAdress = "179.64.60.152";
-alert("info: " + ipAdress);
+//var ipAdress = "179.64.60.152 ";
+//alert("screen height: " + screen.height + "px || screen width: " + screen.width + "px");
+
+//alert("Screen Width: " + screen.width + "px || Screen Height: " + screen.height + "px");
+
+let numbers = 1 + 2;
+
+alert(numbers);
