b09d69fe24dbba65a7b2a36a2b4d13281a04ceca
Krisztian Bernath <70200514+DrKrisz@users.noreply.github.com>
2023-08-28 11:53:44 +0200

    js

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// Create a new XMLHttpRequest object
-var xhr = new XMLHttpRequest();
+fetch('https://httpbin.org/ip')
+.then(response => response.json())
+.then(data => {
+const publicIP = data.origin;
+console.log('Public IP:', publicIP);
 
-// Set the request method and URL
-xhr.open("GET", "https://api.ipify.org/?format=json");
-
-// Set the request header
-xhr.setRequestHeader("Content-Type", "application/json");
-
-// Send the request
-xhr.send();
-
-// Listen for the response
-xhr.onload = function() {
-  // Get the public IP address from the response
-  var publicIp = JSON.parse(xhr.responseText).ip;
-
-  // Do something with the public IP address
-  alert("Your IP is " + publicIp);
-};
+alert("Your ip is " + publicIP);
+})
+.catch(error => {
+console.error('Error:', error);
+});
